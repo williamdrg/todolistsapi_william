b9c2d2a2e5bd4d9faab9f1228640726273d2111d
unknown <davidwdr@ggmail.com>
2023-05-18 15:20:05 -0500

    actualización

--- src/app.js
+++ src/app.js
@@ -9,23 +9,29 @@ initModels();
 
 const app = express();
 app.use(express.json())
-const PORT = process.env.PORT || 3000;
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(userRouters)
 app.use(todoRouters)
 
 
 db.authenticate()
-  .then(() => console.log("Base de datos conectada"))
-  .catch((error) => console.error(error));
+.then(() => console.log("Base de datos conectada"))
+.catch((error) => console.error(error));
 
 db.sync()
-  .then(() => console.log("Base de datos síncronizada"))
-  .catch((error) => console.error(error));
+.then(() => console.log("Base de datos síncronizada"))
+.catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando");
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
